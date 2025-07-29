efac242b6cdbf6f1f88fe7ebddd429f5b5e41cbd
Vimal290704 <vimalyadavkr001@gmail.com>
2025-07-29 19:50:43 +0530

    Initialized backend and frontend devStrike

---
+++ devStrike/src/main/java/com/example/devStrike/DevStrikeApplication.java
@@ -0,0 +1,13 @@
+package com.example.devStrike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevStrikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevStrikeApplication.class, args);
+	}
+
+}
---
+++ devStrike/src/test/java/com/example/devStrike/DevStrikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devStrike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevStrikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
