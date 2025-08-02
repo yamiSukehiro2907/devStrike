a6534ff8fcc9c087d7fc8ce241c15af392857264
Vimal290704 <vimalyadavkr001@gmail.com>
2025-08-02 13:59:39 +0530

    Initialization

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -1,13 +1,13 @@
-package com.example.devStrike;
+package com.example.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DevStrikeApplication {
+public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DevStrikeApplication.class, args);
+		SpringApplication.run(BackendApplication.class, args);
 	}
 
 }
--- backend/src/test/java/com/example/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/example/backend/BackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.devStrike;
+package com.example.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DevStrikeApplicationTests {
+class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
